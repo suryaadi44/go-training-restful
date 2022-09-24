61ab77717e64079c4c07da89b2613efcbb0db6f9
suryaadi44 <suryawarior44@gmail.com>
2022-09-25 03:27:40 +0800

    feat: add user CRUD functionality

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,20 +2,103 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/suryaadi44/go-training-restful/lib/database"
+	"github.com/suryaadi44/go-training-restful/models"
+	"gorm.io/gorm"
 )
 
 func GetUsersController(c echo.Context) error {
 	users, err := database.GetUsers()
 
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status": "success",
-		"data":   users,
+		"message": "success getting users",
+		"data":    users,
+	})
+}
+
+func GetUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	user, err := database.GetUserByID(id)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success getting user",
+		"data":    user,
+	})
+}
+
+func CreateUserController(c echo.Context) error {
+	user := models.User{}
+	c.Bind(&user)
+
+	newUser, err := database.CreateUser(user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success creating user",
+		"data":    newUser,
+	})
+}
+
+func DeleteUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	err = database.DeleteUser(id)
+	if err != nil {
+		if err == database.ErrInvalidID {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success deleting user",
+	})
+}
+
+func UpdateUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	user := models.User{}
+	c.Bind(&user)
+
+	updatedUser, err := database.UpdateUser(id, user)
+	if err != nil {
+		if err == database.ErrInvalidID {
+			return echo.NewHTTPError(http.StatusNotFound, err.Error())
+		}
+
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success updating user",
+		"data":    updatedUser,
 	})
 }
--- lib/database/user.go
+++ lib/database/user.go
@@ -1,8 +1,15 @@
 package database
 
 import (
+	"errors"
+
 	"github.com/suryaadi44/go-training-restful/config"
 	"github.com/suryaadi44/go-training-restful/models"
+)
+
+var (
+	// ErrInvalidID is returned when an invalid ID is provided to a method like Delete.
+	ErrInvalidID = errors.New("provided id was invalid")
 )
 
 func GetUsers() (interface{}, error) {
@@ -14,4 +21,53 @@ func GetUsers() (interface{}, error) {
 	}
 
 	return users, nil
+}
+
+func GetUserByID(id int) (interface{}, error) {
+	var user models.User
+
+	err := config.DB.Where("id = ?", id).First(&user).Error
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func CreateUser(user models.User) (interface{}, error) {
+	err := config.DB.Create(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func DeleteUser(id int) error {
+	var user models.User
+
+	result := config.DB.Where("id = ?", id).Delete(&user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return ErrInvalidID
+	}
+
+	return nil
+}
+
+func UpdateUser(id int, user models.User) (interface{}, error) {
+	result := config.DB.Model(&models.User{}).Where("id = ?", id).Updates(user)
+	if result.Error != nil {
+		return user, result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return user, ErrInvalidID
+	}
+
+	return user, nil
 }
